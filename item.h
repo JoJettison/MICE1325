a07345b82facbc98a01a0142a39432b0a5da825d
JoJettison <jb9013xz@gmail.com>
2017-10-24 15:08:45 -0500

    retCost -> retPri rename

--- item.h
+++ item.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Item
@@ -6,12 +7,12 @@ class Item
 	public:
 		string to_string();
 		Item (string Name, string Description, double wholesaleCost, double retailCost, int remainingStock):
-		name(Name), desc(Description), wholeCost(wholesaleCost), retCost(retailCost), remStock(remainingStock){}
+		name(Name), desc(Description), wholeCost(wholesaleCost), retPri(retailCost), remStock(remainingStock){}
 
 	private:
 		string name;
 		string desc;
 		double wholeCost;
-		double retCost;
+		double retPri;
 		int remStock;
 }
