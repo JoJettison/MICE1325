29df529ef62b6affa9c7eee67ea10790cea7cbca
JoJettison <jb9013xz@gmail.com>
2017-10-25 12:43:55 -0500

    Scoop header and cpp file
    Basic scoop definition, inherits from item. to_string method not working (compile error)

--- scoop.cpp
+++ scoop.cpp
@@ -1,1 +1,10 @@
 #include "scoop.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+void Scoop::to_string(){
+
+  cout<<name<<":"<<" A scoop of "<< desc+" Cost to purchase: "<<wholeCost<<" Sold for "<<retPri<<" RemainingStock: "+remStock<<"scoops"<<endl;
+
+}
--- scoop.h
+++ scoop.h
@@ -1,10 +1,18 @@
+#ifndef SCOOP_H
+#define SCOOP_H
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Scoop
-{
-	public:
+class Scoop: public Item {
 
-	private:
+public:
+	void to_string();
+	Scoop (string Name, string Description, double wholesaleCost, double retailPrice, int remainingStock):
+	Item(Name,Description,wholesaleCost,retailPrice,remainingStock) { }
 
-}
+
+
+
+};
+#endif
