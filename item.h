efa9f472ef290be6f886bb65116b35cba0dd2b86
JoJettison <jb9013xz@gmail.com>
2017-10-30 18:32:33 -0500

    Added getters, prepare for polymorphism

--- item.h
+++ item.h
@@ -8,19 +8,25 @@ using namespace std;
 class Item
 {
 	public:
-		Item (string Name, string Description, double wholesaleCost, double retailPrice, int remainingStock):
-		name{Name}, desc{Description}, wholeCost{wholesaleCost}, retPri{retailPrice}, remStock{remainingStock} { }
+		Item (string Name, string Description, double wholesaleCost, double retailPrice);
+		void restock(int restock=25);
+		void consume(int quantity=1);
+		virtual string type();
+		string iname();
+		string description();
+		double cost();
+		double price();
+		int remainStock();
+
 		void to_string();
 
-		
-
-
-	protected:
+	private:
 		vector<Item> itemList;
 		string name;
 		string desc;
 		double wholeCost;
 		double retPri;
 		int remStock;
+		//Gtk::Image picture
 };
 #endif
--- testItem.cpp
+++ testItem.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 int main(){
 
-Item item1("Apple", "Keeps the doctor away", 2.50, 3.00, 20);
+Item item1("Apple", "Keeps the doctor away", 2.50, 3.00);
 
-
+item1.restock(10);
+item1.to_string();
+item1.consume(5);
+item1.to_string();
 return 0;
 }
